6da35c558f7f9527ffa59ddf5718572e88ab8a8e
Filip Maszota <filipmaszota3@gmail.com>
2023-12-18 19:31:26 +0100

    optimization test

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,16 +1,38 @@
-import React from "react";
-import Image from "next/image";
-import Spline from "@splinetool/react-spline";
-import teestimg from "@/app/teest.png";
+"use client";
+import React, { useEffect, useState } from "react";
 import { useTranslations } from "next-intl";
+import Navbar from "@/components/Navbar";
+import Spline from "@splinetool/react-spline";
 
 export default function Home() {
-  const t = useTranslations("Index");
+  const [isloaded, setisloaded] = useState(false);
+  useEffect(() => {
+    // Add an event listener for the "load" event
+    const handleWindowLoad = () => {
+      // Your code to run after the page has loaded
+      setisloaded(true);
+      console.log("Page loaded");
+    };
+
+    window.addEventListener("load", handleWindowLoad);
+
+    // Clean up the event listener when the component is unmounted
+    return () => {
+      window.removeEventListener("load", handleWindowLoad);
+    };
+  }, []); // The empty dependency array ensures that this effect runs only once, similar to componentDidMount
 
   return (
-    // <Spline scene="https://prod.spline.design/nT3VNbaoR9BeE3b3/scene.splinecode" />
-    // <Spline scene="https://prod.spline.design/Gxsf1S8h9MkHFBaZ/scene.splinecode" />
-
-    <div className="w-full"></div>
+    <div className="w-full">
+      {isloaded ? (
+        "loading"
+      ) : (
+        <>
+          <Navbar />
+          <Spline scene="https://prod.spline.design/nT3VNbaoR9BeE3b3/scene.splinecode" />
+          <Spline scene="https://prod.spline.design/Gxsf1S8h9MkHFBaZ/scene.splinecode" />
+        </>
+      )}
+    </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,17 @@
-import React from "react";
-
+"use client";
+import React, { useState } from "react";
+import logo from "../../public/images/logo.svg";
+import Image from "next/image";
+import Logo from "./Logo";
 interface Props {}
 
 const Navbar = () => {
-  return <div></div>;
+  const [isOpen, setisOpen] = useState(false);
+  return (
+    <nav className="w-full padding h-[80px] fixed top-0 flex items-center justify-between">
+      <Logo />
+    </nav>
+  );
 };
 
 export default Navbar;
