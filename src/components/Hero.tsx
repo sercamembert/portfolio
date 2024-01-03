d5008785b5b8e1d19171b23a6ae515fcfc9e4b3b
Filip Maszota <filipmaszota3@gmail.com>
2024-01-03 19:36:12 +0100

    change size of text in hero section (on mobile)

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -31,7 +31,7 @@ const Hero = ({ text, talk, locale }: Props) => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ duration: 0.3, delay: 0.9 }}
-          className="text-center md:text-left font-medium text-[13.9px] md:text-[14.36px] lg:text-[18.58px] xl:text-[23.7px] 2xl:text-[27.31px] 3xl:text-[27.72px] desktop:text-[30.53px] 
+          className="text-center md:text-left font-medium text-[16.9px] md:text-[14.36px] lg:text-[18.58px] xl:text-[23.7px] 2xl:text-[27.31px] 3xl:text-[27.72px] desktop:text-[30.53px] 
         w-[339px] md:w-[350.3px] lg:w-[453.09px] xl:w-[578.08px] 2xl:w-[666px] 3xl:w-[675.99px] desktop:w-[744.66px] leading-tight
         "
           dangerouslySetInnerHTML={{ __html: coloredText }}
@@ -41,9 +41,9 @@ const Hero = ({ text, talk, locale }: Props) => {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 0.3, delay: 0.6 }}
-            className="bg-white text-black rounded-[10px] xl:rounded-[15px]  h-[40.58px] md:h-[30.1px]  lg:h-[30px]  xl:h-[40px]  2xl:h-[45.12px]  desktop:h-[50.86px]
-        font-semibold text-[16px] md:text-[9px] lg:text-[10.68px] xl:text-[13.62px] 2xl:text-[15.69px] desktop:text-[17.54px] px-3 xl:px-6 hover:brightness-[80%] hover:scale-110 duration-300
-        w-[185.54px] md:w-auto md:min-w-[116.3px] lg:min-w-[150.42px] xl:min-w-[191.92px] 2xl:min-w-[221.11px] 3xl:min-w-[224.42px] desktop:min-w-[247.22px]
+            className="bg-white text-black rounded-[10px] xl:rounded-[15px]  h-[39.58px] md:h-[30.1px]  lg:h-[30px]  xl:h-[40px]  2xl:h-[45.12px]  desktop:h-[50.86px]
+        font-semibold text-[14.9px] md:text-[9px] lg:text-[10.68px] xl:text-[13.62px] 2xl:text-[15.69px] desktop:text-[17.54px] px-3 xl:px-6 hover:brightness-[80%] hover:scale-110 duration-300
+        w-[170.54px] md:w-auto md:min-w-[116.3px] lg:min-w-[150.42px] xl:min-w-[191.92px] 2xl:min-w-[221.11px] 3xl:min-w-[224.42px] desktop:min-w-[247.22px]
         mt-[15px] md:mt-[13px] lg:mt-[16px] xl:mt-[21px] 2xl:mt-[24px] desktop:mt-[27px] "
           >
             {talk}
