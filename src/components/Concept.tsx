0ee981f6d6ea361ede9b5c7fb60a75da5714298f
Filip Maszota <filipmaszota3@gmail.com>
2024-01-03 19:34:54 +0100

    remove concept section from mobile devices

--- src/components/Concept.tsx
+++ src/components/Concept.tsx
@@ -33,7 +33,7 @@ const Concept = () => {
   }, [hasPlayed]);
 
   return (
-    <div className="padding z-0">
+    <div className="padding z-0 hidden md:block">
       <video ref={videoRef} muted className="mx-auto">
         <source
           src="https://cdn.discordapp.com/attachments/1091042166022869143/1189146337820803162/animacja.webm?ex=659d19d4&is=658aa4d4&hm=6aee0a31b6198d7816b0cebf85b2dd8868f6df9eb0703ceed066fac71d7496d6&"
