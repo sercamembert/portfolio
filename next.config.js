de8f4ffc3c289c607b4b6ec96d8f9feb204905b9
Filip Maszota <filipmaszota3@gmail.com>
2024-01-07 15:45:57 +0100

    change next.js config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,3 @@
 const withNextIntl = require("next-intl/plugin")("./src/i18n.ts");
 
-module.exports = withNextIntl({
-  reactStrictMode: true,
-  distDir: "dist",
-});
+module.exports = withNextIntl({});
