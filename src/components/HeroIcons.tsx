11539fc3a3dc6ab664e874e45751a4ef53a2683b
Filip Maszota <filipmaszota3@gmail.com>
2024-01-22 21:20:19 +0100

    change Hero Logos

--- src/components/HeroIcons.tsx
+++ src/components/HeroIcons.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Image from "next/image";
-import flutterflowImg from "../../public/images/hero/flutterflow.png";
-import googleImg from "../../public/images/hero/google.png";
 import quizportalImg from "../../public/images/hero/quizportal.png";
-import microsoftImg from "../../public/images/hero/microsoft.png";
+import realImg from "../../public/images/hero/real.png";
+import recipesImg from "../../public/images/hero/recipes.png";
+import pousImg from "../../public/images/hero/pous.png";
 import { motion } from "framer-motion";
 const HeroIcons = () => {
   return (
@@ -11,33 +11,33 @@ const HeroIcons = () => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.3, delay: 0.3 }}
-      className="md:flex items-center justify-between max-md:flex-wrap hidden gap-[20px] lg:gap-[25px] xl:gap-[33px] 2xl:gap-[37px] desktop:gap-[42px]
+      className="md:flex items-center justify-between hidden gap-[20px] lg:gap-[25px] xl:gap-[33px] 2xl:gap-[37px] desktop:gap-[42px]
       md:mt-[31px] lg:mt-[40px] xl:mt-[51px] 2xl:mt-[59px] desktop:mt-[68px]
 "
     >
       <Image
         loading="lazy"
-        src={flutterflowImg}
-        className="aspect-[4.9] object-contain object-center w-[71.28px] lg:w-[92.19px] xl:w-[117.63px] 2xl:w-[135.52px] 3xl:w-[137.55px] desktop:w-[151.52px]  overflow-hidden shrink-0 max-w-full"
-        alt="flutterflow"
-      />
-      <Image
-        loading="lazy"
-        src={googleImg}
-        className="aspect-[3.13] object-contain object-center overflow-hidden shrink-0 max-w-full w-[45.49px] lg:w-[58.83px] xl:w-[75.07px] 2xl:w-[86.48px] 3xl:w-[87.78px] desktop:w-[96.7px]"
-        alt="google"
-      />
-      <Image
-        loading="lazy"
         src={quizportalImg}
-        className="aspect-[4.84] object-contain object-center  overflow-hidden shrink-0 max-w-full w-[70.81px] lg:w-[92.19px] xl:w-[117.63px] 2xl:w-[135.52px] 3xl:w-[137.55px]"
+        className="w-[77.28px] lg:w-[103.51px] xl:w-[133.14px] 2xl:w-[152.65px] 3xl:w-[163.04px] desktop:w-[170.48px] pt-[5px] xl:pt-[10px] 3xl:pt-[13px]"
         alt="quizportal"
       />
       <Image
         loading="lazy"
-        src={microsoftImg}
-        className="aspect-[4.84] object-contain object-center overflow-hidden shrink-0 max-w-full w-[70.81px] lg:w-[92.19px] xl:w-[117.63px] 2xl:w-[135.52px] 3xl:w-[137.55px]"
-        alt="microsoft"
+        src={realImg}
+        className="w-[35.19px] lg:w-[46.96px] xl:w-[60.41px] 2xl:w-[69.26px] 3xl:w-[73.97px] desktop:w-[77.35px] "
+        alt="real"
+      />
+      <Image
+        loading="lazy"
+        src={recipesImg}
+        className="w-[73.19px] lg:w-[97.94px] xl:w-[126.36px] 2xl:w-[144.93px] 3xl:w-[153.83px] desktop:w-[161.19px]"
+        alt="recipes"
+      />
+      <Image
+        loading="lazy"
+        src={pousImg}
+        className="w-[46px] lg:w-[61px] xl:w-[78px] 2xl:w-[90px] 3xl:w-[96px] desktop:w-[100px] "
+        alt="pous"
       />
     </motion.div>
   );
