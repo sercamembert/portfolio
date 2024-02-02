b8811fa7673f36fa925e9055a5e00be8fc510f37
Filip Maszota <filipmaszota3@gmail.com>
2024-02-02 10:39:49 +0100

    change team

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import shadowImg from "../../public/images/team/shadow.svg";
 import Image from "next/image";
-import FilipImg from "../../public/images/team/Filip.png";
-import MarcelImg from "../../public/images/team/Marcel.png";
+import FilipImg from "../../public/images/team/filip.webp";
+import MarcelImg from "../../public/images/team/marcel.webp";
 interface Props {
   title: string;
 }
@@ -16,13 +16,9 @@ const Team = ({ title }: Props) => {
       <h2 className="font-bold text-[39.02px] md:text-[30px] lg:text-[39px] xl:text-[49.76px] 2xl:text-[52.55px] 3xl:text-[58.19px] desktop:text-[63.73px] text-center md:text-left">
         {title}
       </h2>
-      <div className="flex flex-col md:flex-row gap-y-[50px] justify-between md:gap-x-[127px] lg:gap-x-[163px] xl:gap-x-[208px] 2xl:gap-x-[215px] 3xl:gap-x-[243px] desktop:gap-x-[268px]">
-        <div className="md:w-1/2 z-10 flex flex-col mx-auto">
-          <Image
-            src={FilipImg}
-            alt="Filip Maszota"
-            className="max-w-[360px] md:max-w-full w-full"
-          />
+      <div className="flex flex-col md:flex-row gap-y-[50px] justify-between">
+        <div className="md:w-1/2 z-10 flex flex-col mx-auto md:mx-0 max-w-[360px] md:max-w-[270.8px] lg:max-w-[350px] xl:max-w-[446.88px] 2xl:max-w-[462.24px] 3xl:max-w-[493.7px] desktop:max-w-[575.65px]">
+          <Image src={FilipImg} alt="Filip Maszota" className="w-full" />
           <h3 className="font-semibold text-[20px] md:text-[25.65px] lg:text-[33.18px] xl:text-[42.34px] 3xl:text-[49.51px] desktop:text-[54.53px]">
             Filip Maszota
           </h3>
@@ -34,17 +30,11 @@ const Team = ({ title }: Props) => {
         <Image
           src={shadowImg}
           alt="shadow"
-          className="hidden z-0 absolute md:block left-1/2 -translate-x-1/2
-          
-          "
+          className="hidden z-0 absolute md:block left-1/2 -translate-x-1/2"
         />
-        <div className="md:w-1/2 z-10 flex flex-col mx-auto">
-          <Image
-            src={MarcelImg}
-            alt="Marcel Bogdan"
-            className="max-w-[360px] md:max-w-full w-full"
-          />
-          <h3 className="font-semibold text-[20px] md:text-[25.65px] lg:text-[33.18px] xl:text-[42.34px] 3xl:text-[49.51px] desktop:text-[54.53px]">
+        <div className="w-full z-10 flex flex-col mx-auto md:mx-0 max-w-[360px] md:max-w-[270.8px] lg:max-w-[350px] xl:max-w-[446.88px] 2xl:max-w-[462.24px] 3xl:max-w-[493.7px] desktop:max-w-[575.65px]">
+          <Image src={MarcelImg} alt="Marcel Bogdan" className="w-full" />
+          <h3 className="font-semibold text-[20px] md:text-[25.65px] lg:text-[33.18px] xl:text-[42.34px] 3xl:text-[49.51px] desktop:text-[54.53px] text-left">
             Marcel Bogdan
           </h3>
           <p className="text-[15px] md:text-[17.82px] lg:text-[23.04px] xl:text-[29.4px] 3xl:text-[34.38px] desktop:text-[37.87px]">
