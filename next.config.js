ddcfd10fb387ddac824742df605c0ba827951464
Filip Maszota <filipmaszota3@gmail.com>
2024-02-28 20:46:37 +0100

    try to fix portfolio images bug

--- next.config.js
+++ next.config.js
@@ -3,5 +3,11 @@ const withNextIntl = require("next-intl/plugin")("./src/i18n.ts");
 module.exports = withNextIntl({
   images: {
     domains: ["images.ctfassets.net", "assets.aceternity.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.ctfassets.net",
+      },
+    ],
   },
 });
